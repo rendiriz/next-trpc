98e0ee131b0a74649428e5030f2404e3bfc189ea
Rendi Rizki <rendirizkir@gmail.com>
2022-07-19 00:04:47 +0700

    fix: environment

--- src/server/env-schema.js
+++ src/server/env-schema.js
@@ -4,7 +4,7 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(['development', 'test', 'production']),
   NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.string().url(),
+  NEXTAUTH_URL: z.string().url().nullable(),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
 });
