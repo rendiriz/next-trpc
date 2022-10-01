74f1294bfedc00870a755f225d6cb7345d90eeac
Rendi Rizki <rendirizkir@gmail.com>
2022-10-01 23:41:21 +0700

    fix: upgrade

--- next.config.js
+++ next.config.js
@@ -3,8 +3,12 @@ const { env } = require('./src/server/env');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['raw.githubusercontent.com'],
+  },
+  experimental: {
+    nextScriptWorkers: true,
   },
 };
 
